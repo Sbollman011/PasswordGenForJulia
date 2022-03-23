f09439f5ed12a71a2d2682f75d105bc2b7a28495
Sbollman <steven.bollman@microfocus.com>
2022-03-23 15:05:01 -0700

    Full password generator

--- script.js
+++ script.js
@@ -3,16 +3,38 @@
 function generatePassword() {
     /* make the length equal to the the input with the id="length", not a static input like 10. This way it
         will update as the user interacts with the UI*/
-    var length =  document.getElementById("length").value,
-        charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&*+‑/",
+    var length =  document.getElementById("length").value;
+    var uppercase = document.getElementById("uppercase").checked;
+    var symbols = document.getElementById("symbols").checked;
+    var numbers = document.getElementById("numbers").checked;
+
+    var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&*+‑/",
         retVal = "";
     for (var i = 0, n = charset.length; i < length; ++i) {
-        retVal += charset.charAt(Math.floor(Math.random() * n));
+        let val = charset.charAt(Math.floor(Math.random() * n));
+        if((!uppercase && isUpper(val)) || (!symbols && isSymbol(val)) || (!numbers && isNumber(val))){
+            i--;
+        }
+        else{
+            retVal += val;
+        }
     }
 
     document.getElementById("result").innerText = retVal
 
     return retVal;
+}
+
+function isSymbol(sym){
+    return /[~‑`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/g.test(sym);
+   }
+
+function isNumber(i) {
+    return (i >= '0' && i <= '9');
+}
+
+function isUpper(str){
+    return str == str.toUpperCase();
 }
 
 /*No need for the console print... this was also automatically calling the function everytime the page is opened.
